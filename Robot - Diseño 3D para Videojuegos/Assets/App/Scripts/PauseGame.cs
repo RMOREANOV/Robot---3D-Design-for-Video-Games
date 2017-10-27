8bb31d499617296bbc96d61c27bae600cede9e46
rmoreanov <rmoreanov@users.noreply.github.com>
2017-10-27 16:37:33 -0500

    New Proyect

---
+++ Robot - Diseño 3D para Videojuegos/Assets/App/Scripts/PauseGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+    private void Awake()
+    {
+        Time.timeScale = 0;
+        Debug.Log("Pause");
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Time.timeScale = 1;
+            Debug.Log("Start");
+        }
+    }
+}
